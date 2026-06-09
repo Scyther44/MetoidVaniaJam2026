23fa5971ccba755e0d9c96cc45fbcefcfec60780
Scyther44 <cyberfunkb@gmail.com>
2026-06-08 22:19:38 -0400

    Removed Pausing from pickups to prevent bug Worldbuilding

--- Scripts/pickuppopup.gd
+++ Scripts/pickuppopup.gd
@@ -14,11 +14,11 @@ func show_pickup(
 	body_label.text = text
 	icon.texture = texture
 
-	get_tree().paused = true
+	#get_tree().paused = true
 
 func _unhandled_input(event):
 
 	if event.is_action_pressed("ui_accept") or event.is_action_pressed("ui_cancel"):
 
-		get_tree().paused = false
+		#get_tree().paused = false
 		queue_free()
